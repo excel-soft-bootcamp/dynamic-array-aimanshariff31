e7e8cf03aecd7f1863cd0387ca079286d0ef682a
aimanshariff31 <86050998+aimanshariff31@users.noreply.github.com>
2021-07-05 15:15:58 +0530

    Add files via upload

---
+++ DynamicArrayApp/DynamicNumberArray.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicArrayApp
+{
+    public class DynamicNumberArray
+    {
+
+        int[] buffer;
+        public DynamicNumberArray()
+        {
+            buffer = new int[5];
+        }
+
+        public DynamicNumberArray(int initialSize)
+        {
+            buffer = new int[initialSize];
+        }
+
+        public int this[int index]
+        {
+            
+            set
+            {
+                Array.Resize(ref buffer, index + 5);
+                buffer[index] = value;
+            }
+            get
+            {
+                return buffer[index];
+            }
+
+        }
+    }
+}
+
+
+    
+
+© 2021 GitHub, Inc.
